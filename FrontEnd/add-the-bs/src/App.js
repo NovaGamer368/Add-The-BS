27ab31a56b487851f39c75235f86730ca9f41ed5
G-Illescas <100743577+G-Illescas@users.noreply.github.com>
2024-01-25 20:56:35 -0700

    small changes

--- FrontEnd/add-the-bs/src/App.js
+++ FrontEnd/add-the-bs/src/App.js
@@ -56,11 +56,11 @@ function App() {
                     <h2 className="text-white text-2xl flex">Release Date: </h2>
                     <p className='text-2xl'>{movie.release_date}</p>
                   </div>
-                  {/* <div>
+                  <div>
                     <ul>
                       <li key={movie.id}>{movie.genre_ids.filter(genre_ids => genre_ids !== "").join(" , ")}</li>
                     </ul>
-                  </div> */}
+                  </div>
                     <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                       Read more
                       <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
