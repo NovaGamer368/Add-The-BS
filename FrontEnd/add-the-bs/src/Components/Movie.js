65fdcf04cb41f24855de84e96a4e528a60cbb3c3
cwhited <cwhited@student.neumont.edu>
2024-02-01 18:17:43 -0700

    Adjust from Dex to fix conflicts

--- FrontEnd/add-the-bs/src/Components/Movie.js
+++ FrontEnd/add-the-bs/src/Components/Movie.js
@@ -52,7 +52,8 @@ const Movie = ({ movie }) => {
           </li>
         </ul>
       </div>
-      <Link to={`/MovieInfo/${movie.id}`}
+      <a
+        href={`/MovieInfo/${movie.id}`}
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Read more
@@ -69,7 +70,7 @@ const Movie = ({ movie }) => {
             clipRule="evenodd"
           ></path>
         </svg>
-      </Link>
+       </a>
     </div>
   );
 };
