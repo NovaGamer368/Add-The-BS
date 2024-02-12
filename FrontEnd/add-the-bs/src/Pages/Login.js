533609f516acfd383b95db12a8cf6fcf4d7ccad6
Brennan Phifer <bman5803@gmail.com>
2024-02-12 15:16:39 -0700

    New login screens!

--- FrontEnd/add-the-bs/src/Pages/Login.js
+++ FrontEnd/add-the-bs/src/Pages/Login.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [login, setLogin] = useState(null);
   const navigate = useNavigate();
 
@@ -11,10 +11,10 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3001/login', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3001/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           email: email,
@@ -23,47 +23,82 @@ const Login = () => {
       });
       const data = await response.json();
       if (data.success) {
-        sessionStorage.setItem('sessionKey', data.key);
-        sessionStorage.setItem('userId', data.userId);
-        navigate('/home');
+        sessionStorage.setItem("sessionKey", data.key);
+        sessionStorage.setItem("userId", data.userId);
+        navigate("/home");
       } else {
-        console.log('Login failed:', data.Message);
-        setLogin(false)
+        console.log("Login failed:", data.Message);
+        setLogin(false);
       }
     } catch (error) {
-      console.error('An error occurred during login:', error);
+      console.error("An error occurred during login:", error);
     }
   };
 
   return (
-        <>
-        <div className="App">
-            <header className="App-header">
-                <div>
-                    <h1>Login</h1>
+    <>
+      <div className="App">
+        <div className="h-screen flex flex-col items-center justify-center">
+          <div className="m-auto p-5 w-1/4 border-2 border-white rounded shadow-drawer">
+            <h1 className="text-6xl font-bold mb-3">Cut The BS</h1>
+            <h3 className="text-2xl mb-3 underline">Login</h3>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div class="mb-6">
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
+                  />
                 </div>
-                <div>
-                <form onSubmit={handleSubmit}>
-                        <div class="mb-6">
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="name@example.com" required/>
-                        </div>
-                        <div class="mb-6">
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={password} onChange={(e) => setPassword(e.target.value)}  placeholder="Password" required/>
-                        </div>
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                    </form>
+                <div class="mb-6">
+                  <label
+                    for="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required
+                  />
                 </div>
-                <div>
-                  {login === false && (
-                    <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oh, snapp!</span> User was not found.</p>
-                  )}
+                <div className="mb-3">
+                  <a
+                    className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+                    href="/"
+                  >
+                    Don't have an account?
+                  </a>
                 </div>
-            </header>
+                <button
+                  type="submit"
+                  class="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
